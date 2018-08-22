860243a912fc73636f6768ae697a0d3748914ff3
Mark <markgravitygood@yahoo.com>
2018-08-22 17:47:02 -0400

    Create AddTransportData.sql

---
+++ AddTransportData.sql
@@ -0,0 +1,43 @@
+DECLARE 
+  -- 
+  CURSOR c IS 
+    SELECT cts.claim_transaction_summary_id, 
+           cts.case_detail_id 
+    FROM   claim_transaction_summary cts 
+    WHERE  cts.claim_transaction_summary_id = v_cts_id; 
+
+-- 
+v_cts_id NUMBER;
+-- 
+TYPE t_pt 
+IS 
+  TABLE OF pat_trip%rowtpye INDEX BY pls_integer; 
+  -- 
+  a_pt T_PT := T_pt(); 
+  -- 
+TYPE t_ptl 
+IS 
+  TABLE OF pat_trip_leg%ROWTYPE INDEX BY PLS_INTEGER; 
+  -- 
+  a_ptl T_PTL := T_ptl(); 
+  -- 
+TYPE t_crc 
+IS 
+  TABLE OF conditions_indicator_crc%ROWTYPE INDEX BY PLS_INTEGER; 
+  -- 
+  a_crc T_CRC := T_crc(); 
+  -- 
+TYPE t_k3 
+IS 
+  TABLE OF k3_file_info%ROWTYPE INDEX BY PLS_INTEGER; 
+  -- 
+  a_k3 T_K3 := T_k3(); 
+  -- 
+BEGIN 
+  FOR rec IN c 
+  LOOP 
+  END LOOP; 
+EXCEPTION 
+WHEN OTHERS THEN 
+  raise_application_error(-20001, SQLERRM); 
+END;
